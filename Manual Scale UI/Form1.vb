11ef59facbf12af47d6a034cb5bdb4eaa8dbce15
gvflowtechnologies <gvoss@tflow.net>
2016-05-24 17:16:13 -0500

    Pete 24May 5pm

--- Manual Scale UI/Form1.vb
+++ Manual Scale UI/Form1.vb
@@ -428,7 +428,7 @@ Public Class Manual_Weight
 
         'Cycle through cylinders in pallet
 
-        For r = 0 To 4 'activePallet.rows - 1
+        For r = 0 To ActivePallet.rows - 1
 
             If r > 10 Then
                 If ActivePallet.Palletlocation = PalletData.PLocation.PalletLeft Then
@@ -438,7 +438,7 @@ Public Class Manual_Weight
                 End If
             End If
 
-            For c = 0 To 4 'ActivePallet.columns - 1
+            For c = 0 To ActivePallet.columns - 1
 
                 '************************************
                 'Stop measuring if the scale is bad.
