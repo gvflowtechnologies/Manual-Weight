a12a57c03a9e2c5b6eff3b7b81fa544e195eae64
Gregory Voss <gvoss@tflow.net>
2016-05-25 19:18:56 -0500

    updated

--- Manual Scale UI/Form1.vb
+++ Manual Scale UI/Form1.vb
@@ -550,22 +550,22 @@ Public Class Manual_Weight
                                 Thread.Sleep(1)
                             Loop
 
-                    If pauserequest = True Then Controlled_Pause()
-                    ' 10 Pick up part from scale
-                    pickscalepart(leftyrighty)
+                            If pauserequest = True Then Controlled_Pause()
+                            ' 10 Pick up part from scale
+                            pickscalepart(leftyrighty)
 
-                    If pauserequest = True Then Controlled_Pause()
-                Else 'If no cylinder was PICKED in the spot set the weight to -10 (flag for no part)
-                    NOCYLINDER()
-                End If
+                            If pauserequest = True Then Controlled_Pause()
+                        Else 'If no cylinder was PICKED in the spot set the weight to -10 (flag for no part)
+                            NOCYLINDER()
+                        End If
 
                     Else
-                'If no cylinder was detected in the spot set the weight to -10 (flag for no part)
-                NOCYLINDER()
+                        'If no cylinder was detected in the spot set the weight to -10 (flag for no part)
+                        NOCYLINDER()
 
                     End If
                 Else
-                Picked = False
+                    Picked = False
                 End If
 
                 '10 Move to spot (either Pallet/Good/Bad) - Seperate Routine
