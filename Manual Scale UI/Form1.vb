d5d9e96734e32cdb56a09da98a935e5dae9d369a
Gvflowtechnologies <gvoss@tflow.net>
2025-07-29 16:17:02 -0500

    Revised preweight supervisoer approval.  Reverting test state to idle.

--- Manual Scale UI/Form1.vb
+++ Manual Scale UI/Form1.vb
@@ -224,9 +224,13 @@ Public Class Manual_Weight
                     Else 'If this is a first weight, check for a duplicate serial number
 
                         If MDataset.SN_Already_Exists(ccylinder.SerialNumber) Then  ' Serial number is a duplicate
-                            ccylinder.Firstweight = -20
-                            Disposition() 'duplicate serial number.
-                            teststate = Weighprocess.prompting
+
+                            Do
+                                login = InputBox("Supervisor Approval Required", "Error - Wrong SN Prefix", "")
+
+                            Loop Until login = My.Settings.Password
+
+                            teststate = Weighprocess.idle
                         End If
                     End If
                 End If
